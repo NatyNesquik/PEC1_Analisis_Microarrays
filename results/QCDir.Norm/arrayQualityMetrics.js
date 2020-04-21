b93f925941d112d81c6b251999dc61427495b89c
NatyNesquik <61521768+NatyNesquik@users.noreply.github.com>
2020-04-21 14:59:37 +0200

    Add files via upload

---
+++ results/QCDir.Norm/arrayQualityMetrics.js
@@ -0,0 +1,189 @@
+// (C) Wolfgang Huber 2010-2011
+
+// Script parameters - these are set up by R in the function 'writeReport' when copying the 
+//   template for this script from arrayQualityMetrics/inst/scripts into the report.
+
+var highlightInitial = [ false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false ];
+var arrayMetadata    = [ [ "1", "M121_Smoke", "M121_Smoke", "Smoke_Male", "Macho" ], [ "2", "M191_Smoke", "M191_Smoke", "Smoke_Male", "Macho" ], [ "3", "M205_Smoke", "M205_Smoke", "Smoke_Male", "Macho" ], [ "4", "M207_Smoke", "M207_Smoke", "Smoke_Male", "Macho" ], [ "5", "M443_Smoke", "M443_Smoke", "Smoke_Male", "Macho" ], [ "6", "M447_Smoke", "M447_Smoke", "Smoke_Male", "Macho" ], [ "7", "F195_Smoke", "F195_Smoke", "Smoke_Female", "Hembra" ], [ "8", "F197_Smoke", "F197_Smoke", "Smoke_Female", "Hembra" ], [ "9", "F201_Smoke", "F201_Smoke", "Smoke_Female", "Hembra" ], [ "10", "F202_Smoke", "F202_Smoke", "Smoke_Female", "Hembra" ], [ "11", "F444_Smoke", "F444_Smoke", "Smoke_Female", "Hembra" ], [ "12", "F445_Smoke", "F445_Smoke", "Smoke_Female", "Hembra" ], [ "13", "M141_Control", "M141_Control", "Control_Male", "Macho" ], [ "14", "M187_Control", "M187_Control", "Control_Male", "Macho" ], [ "15", "M307_Control", "M307_Control", "Control_Male", "Macho" ], [ "16", "M386_Control", "M386_Control", "Control_Male", "Macho" ], [ "17", "M421_Control", "M421_Control", "Control_Male", "Macho" ], [ "18", "F181_Control", "F181_Control", "Control_Female", "Hembra" ], [ "19", "F301_Control", "F301_Control", "Control_Female", "Hembra" ], [ "20", "F381_Control", "F381_Control", "Control_Female", "Hembra" ], [ "21", "F427_Control", "F427_Control", "Control_Female", "Hembra" ] ];
+var svgObjectNames   = [ "pca", "dens" ];
+
+var cssText = ["stroke-width:1; stroke-opacity:0.4",
+               "stroke-width:3; stroke-opacity:1" ];
+
+// Global variables - these are set up below by 'reportinit'
+var tables;             // array of all the associated ('tooltips') tables on the page
+var checkboxes;         // the checkboxes
+var ssrules;
+
+
+function reportinit() 
+{
+ 
+    var a, i, status;
+
+    /*--------find checkboxes and set them to start values------*/
+    checkboxes = document.getElementsByName("ReportObjectCheckBoxes");
+    if(checkboxes.length != highlightInitial.length)
+	throw new Error("checkboxes.length=" + checkboxes.length + "  !=  "
+                        + " highlightInitial.length="+ highlightInitial.length);
+    
+    /*--------find associated tables and cache their locations------*/
+    tables = new Array(svgObjectNames.length);
+    for(i=0; i<tables.length; i++) 
+    {
+        tables[i] = safeGetElementById("Tab:"+svgObjectNames[i]);
+    }
+
+    /*------- style sheet rules ---------*/
+    var ss = document.styleSheets[0];
+    ssrules = ss.cssRules ? ss.cssRules : ss.rules; 
+
+    /*------- checkboxes[a] is (expected to be) of class HTMLInputElement ---*/
+    for(a=0; a<checkboxes.length; a++)
+    {
+	checkboxes[a].checked = highlightInitial[a];
+        status = checkboxes[a].checked; 
+        setReportObj(a+1, status, false);
+    }
+
+}
+
+
+function safeGetElementById(id)
+{
+    res = document.getElementById(id);
+    if(res == null)
+        throw new Error("Id '"+ id + "' not found.");
+    return(res)
+}
+
+/*------------------------------------------------------------
+   Highlighting of Report Objects 
+ ---------------------------------------------------------------*/
+function setReportObj(reportObjId, status, doTable)
+{
+    var i, j, plotObjIds, selector;
+
+    if(doTable) {
+	for(i=0; i<svgObjectNames.length; i++) {
+	    showTipTable(i, reportObjId);
+	} 
+    }
+
+    /* This works in Chrome 10, ssrules will be null; we use getElementsByClassName and loop over them */
+    if(ssrules == null) {
+	elements = document.getElementsByClassName("aqm" + reportObjId); 
+	for(i=0; i<elements.length; i++) {
+	    elements[i].style.cssText = cssText[0+status];
+	}
+    } else {
+    /* This works in Firefox 4 */
+    for(i=0; i<ssrules.length; i++) {
+        if (ssrules[i].selectorText == (".aqm" + reportObjId)) {
+		ssrules[i].style.cssText = cssText[0+status];
+		break;
+	    }
+	}
+    }
+
+}
+
+/*------------------------------------------------------------
+   Display of the Metadata Table
+  ------------------------------------------------------------*/
+function showTipTable(tableIndex, reportObjId)
+{
+    var rows = tables[tableIndex].rows;
+    var a = reportObjId - 1;
+
+    if(rows.length != arrayMetadata[a].length)
+	throw new Error("rows.length=" + rows.length+"  !=  arrayMetadata[array].length=" + arrayMetadata[a].length);
+
+    for(i=0; i<rows.length; i++) 
+ 	rows[i].cells[1].innerHTML = arrayMetadata[a][i];
+}
+
+function hideTipTable(tableIndex)
+{
+    var rows = tables[tableIndex].rows;
+
+    for(i=0; i<rows.length; i++) 
+ 	rows[i].cells[1].innerHTML = "";
+}
+
+
+/*------------------------------------------------------------
+  From module 'name' (e.g. 'density'), find numeric index in the 
+  'svgObjectNames' array.
+  ------------------------------------------------------------*/
+function getIndexFromName(name) 
+{
+    var i;
+    for(i=0; i<svgObjectNames.length; i++)
+        if(svgObjectNames[i] == name)
+	    return i;
+
+    throw new Error("Did not find '" + name + "'.");
+}
+
+
+/*------------------------------------------------------------
+  SVG plot object callbacks
+  ------------------------------------------------------------*/
+function plotObjRespond(what, reportObjId, name)
+{
+
+    var a, i, status;
+
+    switch(what) {
+    case "show":
+	i = getIndexFromName(name);
+	showTipTable(i, reportObjId);
+	break;
+    case "hide":
+	i = getIndexFromName(name);
+	hideTipTable(i);
+	break;
+    case "click":
+        a = reportObjId - 1;
+	status = !checkboxes[a].checked;
+	checkboxes[a].checked = status;
+	setReportObj(reportObjId, status, true);
+	break;
+    default:
+	throw new Error("Invalid 'what': "+what)
+    }
+}
+
+/*------------------------------------------------------------
+  checkboxes 'onchange' event
+------------------------------------------------------------*/
+function checkboxEvent(reportObjId)
+{
+    var a = reportObjId - 1;
+    var status = checkboxes[a].checked;
+    setReportObj(reportObjId, status, true);
+}
+
+
+/*------------------------------------------------------------
+  toggle visibility
+------------------------------------------------------------*/
+function toggle(id){
+  var head = safeGetElementById(id + "-h");
+  var body = safeGetElementById(id + "-b");
+  var hdtxt = head.innerHTML;
+  var dsp;
+  switch(body.style.display){
+    case 'none':
+      dsp = 'block';
+      hdtxt = '-' + hdtxt.substr(1);
+      break;
+    case 'block':
+      dsp = 'none';
+      hdtxt = '+' + hdtxt.substr(1);
+      break;
+  }  
+  body.style.display = dsp;
+  head.innerHTML = hdtxt;
+}
